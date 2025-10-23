c27577e3c0575f57f640c9756484aa74d1a7576e
Andrey Shurygin <andrey.sh@solargroup.team>
2025-10-23 17:53:01 +0300

    Fix

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
+import ProfileAvatar from '@/components/ProfileAvatar.vue';
 import MeditationList from '@/components/MeditationList.vue';
 </script>
 
 <template>
   <div class="grid grid-cols-[370px_1fr]">
-    <div></div>
+    <div>
+      <ProfileAvatar />
+    </div>
     <div><MeditationList /></div>
   </div>
 </template>
